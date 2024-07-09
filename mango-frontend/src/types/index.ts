ab6194a8ccf1b23ad8fb1cf4cbb081eed115cf3b
Himanshu Sharma <himanshu.kumarr07@gmail.com>
2024-07-09 22:30:42 +0530

    prisma removed

--- mango-frontend/src/types/index.ts
+++ mango-frontend/src/types/index.ts
@@ -1,4 +1,12 @@
-import { Sprint } from "@prisma/client";
+export interface Sprint {
+  sprint_id: number;
+  name: string;
+  description?: string;
+  start_date: string;
+  end_date: string;
+  status: 'ongoing' | 'upcoming' | 'completed';
+  organizationId: number;
+}
 
 export interface FetchSprintsPayload {
   sprints: Sprint[];
